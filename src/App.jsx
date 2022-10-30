6d5e485689bcec30cc4f0deab0c81274a6e77f14
Virendra Khorwal <virendrakhorwalvk@gmail.com>
2022-10-30 12:28:23 +0530

    initial setup

---
+++ src/App.jsx
@@ -0,0 +1,15 @@
+import { useState } from 'react'
+import './App.css'
+
+function App() {
+
+  return (
+    <div className="App">
+      <h className="text-primary">
+        Hello World!
+      </h>
+    </div>
+  )
+}
+
+export default App
---
+++ tailwind.config.cjs
@@ -0,0 +1,26 @@
+/** @type {import('tailwindcss').Config} */
+module.exports = {
+  content: [
+    "./index.html",
+    "./src/**/*.{js,ts,jsx,tsx}",
+  ],
+  daisyui: {
+    themes: [
+      {
+        mytheme: {
+
+          "primary": "#5773ff",
+          "secondary": "#596abd",
+          "accent": "#1FB2A6",
+          "neutral": "#18181d",
+          "base-100": "#060606",
+          "info": "#3ABFF8",
+          "success": "#36D399",
+          "warning": "#FBBD23",
+          "error": "#F87272",
+        },
+      },
+    ],
+  },
+  plugins: [require("daisyui")],
+}
